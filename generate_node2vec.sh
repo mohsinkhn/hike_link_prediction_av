0ce7283d8be3643791441f46e29dccdd2d016e1a
catatonic2aucourant <mohsinjackspiro@gmail.com>
2019-04-10 22:46:57 +0530

    script to generate node2vec features

---
+++ generate_node2vec.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/snap-stanford/snap.git
+cd snap
+make all
+cd examples/node2vec
+./node2vec -i:../../../utility/weighted_nodes.graph -o:../../../utility/hike_node2vec.emb -v -l:21 -d:32 -p:0.3 -e:2 -k:4
+cd ../../..
